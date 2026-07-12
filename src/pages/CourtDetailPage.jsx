f7d79e3e83f62e83b4ea70729dcda266f30ecdf6
Vince Gabrielle Milos <gabriellodavinci@icloud.com>
2026-07-13 03:49:49 +0800

    fix court detail image URL for Cloudinary

--- src/pages/CourtDetailPage.jsx
+++ src/pages/CourtDetailPage.jsx
@@ -82,7 +82,7 @@ function CourtDetailPage() {
             court.images.slice(0, 3).map((img, i) => (
               <img
                 key={i}
-                src={court.images[0].imageUrl}
+                src={img.imageUrl}
                 style={{ width: '100%', height: '100%', objectFit: 'cover' }}
                 alt="court"
               />
